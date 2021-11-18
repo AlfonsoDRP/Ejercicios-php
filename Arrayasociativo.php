aa719bf4906aa7a7e4ac8bb76755e9a62ae22cdc
Alfonso <alfonsoderojasperez@gmail.com>
2021-11-18 10:16:28 +0100

    clase 18/11/2021

---
+++ Arrayasociativo.php
@@ -0,0 +1,23 @@
+<?php
+$array= array("Paco","María","Pedro");
+echo("---------------------<br>");
+var_dump($array);
+$nombreFemeninos = array (
+    1 => "Lucia",
+    2 => "Juani",
+    10 => "Rosa"
+);
+echo("<br>---------------------<br>");
+var_dump($nombreFemeninos);
+$nombreFemeninos[10]="Bélen";
+echo("<br>---------------------<br>");
+var_dump($nombreFemeninos);
+$caractPersona = array(
+    "pelo"=>"marron",
+    "ojos"=>"azules",
+    "altura"=>1.75,
+    5=>10
+);
+echo("<br>---------------------<br>");
+var_dump($caractPersona);
+?>
---
+++ creacionArray.php
@@ -0,0 +1,10 @@
+<?php
+$alturasPrimero=array(164,178,169,182);
+$alturassegundo=array(
+    0=>164,
+    1=>178,
+    2=>169,
+    3=>182
+);
+echo key($alturassegundo);
+?>
